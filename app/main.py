d8957ff1927496556000964fb82d488090637b0d
EduardoHerreraM <eduxtreme16@gmail.com>
2023-11-24 19:29:48 +0100

    feat: Complete the main logic, reading the configuration values from file and writing the output to a file

---
+++ app/main.py
@@ -0,0 +1,39 @@
+import json
+
+from src.infrastructure.mosquitto_mqtt_client import MosquittoMQTTClient
+from src.application.generate_climate_json import GenerateClimateJson
+from src.domain.controllers.moes_ufo_r11_controller import MOES_UFO_R11_Controller
+from settings import Settings
+
+
+if __name__ == "__main__":
+
+    settings = Settings(_env_file="properties.env")
+
+    controller = MOES_UFO_R11_Controller(
+        controller_read_code_topic=f"zigbee2mqtt/{settings.CONTROLLER_FRIENDLY_NAME}",
+        controller_set_learning_topic=f"zigbee2mqtt/{settings.CONTROLLER_FRIENDLY_NAME}/set"
+    )
+    mqtt_client = MosquittoMQTTClient(
+        broker_address=settings.BROKER_ADDRESS,
+        broker_port=settings.BROKER_PORT,
+        username=settings.USERNAME,
+        password=settings.PASSWORD,
+    )
+    generate_climate_json = GenerateClimateJson(
+        mqtt_client=mqtt_client,
+        controller=controller
+    )
+
+    codes = generate_climate_json(
+        manufacturer=settings.MANUFACTURER,
+        model=settings.MODEL,
+        minimum_temperature=settings.MINIMUM_TEMPERATURE,
+        maximum_temperature=settings.MAXIMUM_TEMPERATURE,
+        operation_modes=settings.OPERATION_MODES,
+        fan_modes=settings.FAN_MODES,
+        swing_modes=settings.SWING_MODES,
+    )
+
+    with open(f"{settings.MANUFACTURER}-{settings.MODEL}-codes.json", "w") as f:
+        json.dump(codes, f, indent=4, default=str)
